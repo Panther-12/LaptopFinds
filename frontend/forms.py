1865b7bba8f9231a3070ecd45e3ee499e7059d45
Panther-12 <nimrodwalwe@gmail.com>
2023-08-26 19:46:46 +0300

    logo and description section

--- frontend/forms.py
+++ frontend/forms.py
@@ -7,14 +7,16 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
+	phone_number = forms.CharField(required=True)
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username", "email", "last_name", "password1", "password2")
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
 		user.email = self.cleaned_data['email']
+		user.phone_number = self.cleaned_data['last_name']
 		if commit:
 			user.save()
 		return user
